b1c914c8b2d431b43940ff6ec5b25bd7d6f7004a
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 13:29:30 +0530

    Add files via upload

---
+++ TALLER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,x,y;
+	cin>>t;
+	for(i=0; i<t;i++)
+	{
+	    cin>>x>>y;
+	    if(x>y)
+	    cout<<"A"<<endl;
+	    else
+	    cout<<"B"<<endl;
+	    
+	}// your code goes here
+	return 0;
+}
