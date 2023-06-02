f496838a689b06dfd9738bd001a26e26b6884d46
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 13:28:29 +0530

    Add files via upload

---
+++ REACHTARGET.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,x,y;
+	cin>>t;
+	for(i=0; i<t;i++)
+	{
+	    int RunstoWin;
+	    cin>>x>>y;
+	    RunstoWin=x-y;
+	    cout<<RunstoWin<<endl;
+	}// your code goes here
+	return 0;
+}
