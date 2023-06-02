6147602ad1b4971eef62d5ab40dbcc2f354b62c3
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:45:13 +0530

    Add files via upload

---
+++ BIRYANI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    int TotalMoney=x*y;
+	    cout<<TotalMoney<<endl;
+	}// your code goes here
+	return 0;
+}
