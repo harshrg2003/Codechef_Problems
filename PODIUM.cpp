6459671692eb351931bb66e0f5fc1e80f588d064
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:10:51 +0530

    Add files via upload

---
+++ PODIUM.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b;
+	    int TimeGapCHEFWINNER=a+b;
+	    cout<<TimeGapCHEFWINNER<<endl;
+	}// your code goes here
+	return 0;
+}
