85d9d5d42776559ecf56261be511b1f6fd863241
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:25:39 +0530

    Add files via upload

---
+++ SEMCOURSES.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y,z,totalchapter;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y>>z;
+	    totalchapter=x*y*z;
+	    cout<<totalchapter<<endl;
+	}// your code goes here
+	return 0;
+}
