38dd3954240900271fac32b9194151705eea3ae2
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:14:42 +0530

    Add files via upload

---
+++ SHOPCHANGE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    int change;
+	    change=100-x;
+	    cout<<change<<endl;
+	}// your code goes here
+	return 0;
+}
