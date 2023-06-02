9f63b1c85027980749bc98acb39305941115b9e5
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:26:16 +0530

    Add files via upload

---
+++ WATERREQ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n,cooltime;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cooltime=n*2;
+	    cout<<cooltime<<endl;
+	}// your code goes here
+	return 0;
+}
