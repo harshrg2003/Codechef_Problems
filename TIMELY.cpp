0e85eb9c9a330932ee3c79c0b9ecc7b5eea0d0a6
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 13:30:22 +0530

    Add files via upload

---
+++ TIMELY.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,x;
+	cin>>t;
+	for(i=0; i<t;i++)
+	{
+	    cin>>x;
+	    if(x>=30)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}// your code goes here
+	return 0;
+}
