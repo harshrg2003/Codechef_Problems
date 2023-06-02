f7cee41e6325ebd5beca9467305ab20b95834a03
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:47:42 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    int Totalkm=2*5*x;
+	    cout<<Totalkm<<endl;
+	}// your code goes here
+	return 0;
+}
