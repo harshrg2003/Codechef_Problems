d576459e87a6a2552ea3c44693752a14165b2911
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:59:18 +0530

    Add files via upload

---
+++ FBC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,k,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>k>>x;
+	    int litre=k-x;
+	    cout<<litre<<endl;
+	}// your code goes here
+	return 0;
+}
