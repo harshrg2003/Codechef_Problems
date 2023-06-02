7152b911d598dcede5ecf02f2e82be09c6157e62
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:12:39 +0530

    Add files via upload

---
+++ OFFICE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	   int WorkinghourMONTHU=4*x;
+	   int TotalWORKHOUR=WorkinghourMONTHU+y;
+	   cout<<TotalWORKHOUR<<endl;
+	}// your code goes here
+	return 0;
+}
