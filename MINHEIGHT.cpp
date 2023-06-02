5f94faca93574e86aa60c7cb5e4e4ef3e0a99f73
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 13:24:51 +0530

    Add files via upload

---
+++ MINHEIGHT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,x,h;
+	cin>>t;
+	for(i=0; i<t;i++)
+	{
+	    cin>>x>>h;
+	    if(x>=h)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}// your code goes here
+	return 0;
+}
