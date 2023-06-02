3402ef523efcf3cd8936c2eff79c4744c9ccbfa4
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-02 16:11:13 +0530

    Add files via upload

---
+++ FAIRPASS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n,k;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>k;
+	    if(k>n)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}// your code goes here
+	return 0;
+}
