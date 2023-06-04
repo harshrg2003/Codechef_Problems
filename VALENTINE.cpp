857c222a371c9d9151e9b4ac4dc969f8b5fb0abc
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-04 18:06:56 +0530

    Add files via upload

---
+++ VALENTINE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    int max_chocolates=x/y;
+	    cout<<max_chocolates<<endl;
+	}// your code goes here
+	return 0;
+}
