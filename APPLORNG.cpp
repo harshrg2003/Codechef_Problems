2306dfb704ea621b560cb29171758766f0c24906
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-04 18:34:03 +0530

    Add files via upload

---
+++ APPLORNG.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,a,b;
+    cin>>x>>a>>b;
+    if(x>=(a+b))
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }// your code goes here
+	return 0;
+}
