cbf6bcdbf78eb6f51a4c66b752cd952678952e40
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-04 18:36:58 +0530

    Add files via upload

---
+++ CARTRIP.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x<=300)
+	    {
+	        cout<<(300*10)<<endl;
+	    }
+	    else
+	    {
+	        cout<<(x*10)<<endl;
+	    }
+	}// your code goes here
+	return 0;
+}
